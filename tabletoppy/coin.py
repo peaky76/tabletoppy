6ab9bc8ec10f3a84f391684c7fc996e1825f714a
peaky76 <robertjamespeacock@gmail.com>
2023-02-13 20:25:11 +0000

    amend docstrings to sphinx format

--- tabletoppy/coin.py
+++ tabletoppy/coin.py
@@ -4,49 +4,39 @@ from typing import Optional
 
 
 class Coin:
-    """
-    A class used to represent a coin with two sides, heads and tails
-
-    ...
-
-    Attributes
-    ----------
-    face : Face
-        The face of the coin currently displayed to the world
-    """
+    """A class used to represent a coin with two sides, heads and tails"""
 
     class Face(Enum):
-        """
-        An Enum class representing the two sides of a coin
-        """
+        """An Enum class representing the two sides of a coin"""
 
         HEADS = 1
         TAILS = 2
 
     def __init__(self, face: Optional[Face] = None):
-        """
-        Parameters
-        ----------
-        face : Face, optional
-            The face currently displayed to the world (default will call setter which selects heads or tails at random)
+        """Initializer for a coin object
+
+        :param face: The face currently displayed to the world, defaults to None, which will result in heads or tails being set at random
+        :type face: Optional[Face], optional
         """
         self.face = face
 
+    def __str__(self):
+        pass
+
     @property
     def face(self) -> Face:
-        """
-        Getter for the face attribute
+        """Getter for the face attribute
+
+        :return: The face currently displayed to the world
+        :rtype: Face
         """
         return self._face
 
     @face.setter
     def face(self, value: Optional[Face] = None) -> None:
-        """
-        Setter for the face attribute
+        """Setter for the face attribute
 
-        Parameters
-        ----------
-        face : Face, optional
-            The face currently displayed to the world (default of None will set heads or tails at random)
+        :param value: The face currently displayed to the world, defaults to None, which will result in heads or tails being set at random
+        :type value: Optional[Face], optional
         """
         self._face = value or Coin.Face(random.randint(1, 2))
