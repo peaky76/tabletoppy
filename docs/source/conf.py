b0848eec83492d6aca5758cf44276641a35057f5
peaky76 <robertjamespeacock@gmail.com>
2023-02-14 00:15:46 +0000

    add and configure sphinx for docs

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../tabletoppy"))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Tabletoppy"
+copyright = "2023, Robert Peacock"
+author = "Robert Peacock"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
