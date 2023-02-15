9b37edebdd9105e29208a90826beb4ad8256a766
peaky76 <robertjamespeacock@gmail.com>
2023-02-15 20:17:39 +0000

    feat: add .face property to Die

--- tabletoppy/die.py
+++ tabletoppy/die.py
@@ -1,3 +1,4 @@
+import random
 from typing import Optional
 
 
@@ -9,3 +10,22 @@ class Die:
         :type sides: Optional[int], optional
         """
         self.sides = sides
+        self.face = None
+
+    @property
+    def face(self) -> int:
+        """Getter for the face attribute
+
+        :return: The current uppermost face of the die
+        :rtype: int
+        """
+        return self._face
+
+    @face.setter
+    def face(self, value: Optional[int] = None) -> None:
+        """Setter for the face attribute
+
+        :param value: The current uppermost face of the die, defaults to None, which will set it randomly
+        :type value: Optional[int], optional
+        """
+        self._face = value or random.randint(1, self.sides)
--- tests/test_die.py
+++ tests/test_die.py
@@ -9,3 +9,22 @@ def test_initialize_die_with_default():
 def test_initialize_die_with_sides():
     die = Die(8)
     assert 8 == die.sides
+
+
+def test_initialize_randomly_determines_face(mocker):
+    mock_random = mocker.patch("tabletoppy.die.random")
+    Die()
+    mock_random.randint.assert_called_with(1, 6)
+
+
+def test_initialize_uses_sides_as_randint_limit(mocker):
+    mock_random = mocker.patch("tabletoppy.die.random")
+    Die(8)
+    mock_random.randint.assert_called_with(1, 8)
+
+
+def test_initialize_sets_face_as_randomly_generated(mocker):
+    mock_random = mocker.patch("tabletoppy.die.random")
+    mock_random.randint.return_value = 4
+    die = Die()
+    assert 4 == die.face
