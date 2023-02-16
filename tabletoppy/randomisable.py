f0e4a0784f16e5116378d4dee60f4174591e3097
peaky76 <robertjamespeacock@gmail.com>
2023-02-16 23:17:54 +0000

    refactor: add Randomisable abstract base class

---
+++ tabletoppy/randomisable.py
@@ -0,0 +1,52 @@
+from abc import ABC
+from enum import Enum
+import random
+from typing import Optional, Type, TypeVar
+
+_E = TypeVar("_E", bound=Enum)
+
+
+class Randomisable(ABC):
+    def __init__(self, options: Type[_E], selection: Optional[_E] = None):
+        """Abstract base class for any object that can be randomised to create a selection, such as a coin (by tossing), a dice (by rolling) or a card (by drawing)
+
+        :param options: An Enum representing the range of values from which the selection will be made
+        :type options: Enum
+        :param selection: The currently selected value, defaults to None, which will result in a value being selected at random
+        :type selection: Optional[Enum], optional
+        """
+        self.options = options
+        self.selection = selection
+
+    @property
+    def selection(self) -> _E:
+        """Getter for the current selection
+
+        :return: The currently selected option
+        :rtype: Enum
+        """
+        return self._selection
+
+    @selection.setter
+    def selection(self, value: Optional[Enum] = None) -> None:
+        """Setter for the current selection
+
+        :param value: The option which will become the new selection, defaults to None at which point an option will be selected at random
+        :type value: Optional[Enum], optional
+        :raises TypeError: if a suitable option has not been given
+        """
+        if value and not isinstance(value, Enum):
+            raise TypeError(f"{value} is not an instance of {self.options}")
+        self._selection = value or self.options(self._randomiser())
+
+    def _randomiser(self) -> int:
+        """Private function to generate a integer corresponding to one of the options
+
+        :return: An integer corresponding to a value in the options
+        :rtype: int
+        """
+        return random.randint(1, len(self.options))
+
+    def _reset(self) -> None:
+        """Private function to set selection to None, thereby prompting it to be randomised"""
+        self.selection = None
---
+++ tests/test_randomisable.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+import pytest
+from tabletoppy.randomisable import Randomisable
+
+Foobar = Enum("Foobar", ["ALPHA", "BETA", "GAMMA"])
+
+
+def test_initialize_randomisable_correctly_sets_options():
+    randomisable = Randomisable(Foobar)
+    assert Foobar == randomisable.options
+
+
+def test_initialize_randomisable_correctly_sets_selection():
+    randomisable = Randomisable(Foobar, Foobar.ALPHA)
+    assert Foobar.ALPHA == randomisable.selection
+
+
+def test_initialize_randomisable_with_invalid_selection_raises_type_error():
+    with pytest.raises(TypeError):
+        Randomisable(Foobar, 6)
+
+
+def test_initialize_randomisable_determines_selection_using_randomiser(mocker):
+    mock_randomiser = mocker.patch("tabletoppy.randomisable.Randomisable._randomiser")
+    mock_randomiser.return_value = 1
+    Randomisable(Foobar)
+    mock_randomiser.assert_called()
+
+
+def test_initialize_randomisable_determines_selection_as_randomly_generated(mocker):
+    mock_randomiser = mocker.patch("tabletoppy.randomisable.Randomisable._randomiser")
+    mock_randomiser.return_value = 2
+    randomisable = Randomisable(Foobar)
+    assert Foobar.BETA == randomisable.selection
+
+
+def test_randomiser_generates_random_integer_with_options_as_limit(mocker):
+    mock_random = mocker.patch("tabletoppy.randomisable.random")
+    mock_random.randint.return_value = 1
+    randomisable = Randomisable(Foobar)
+    randomisable._randomiser()
+    mock_random.randint.assert_called_with(1, len(randomisable.options))
+
+
+def test_reset_invokes_randomiser(mocker):
+    randomisable = Randomisable(Foobar)
+    mock_randomiser = mocker.patch("tabletoppy.randomisable.Randomisable._randomiser")
+    mock_randomiser.return_value = 1
+    randomisable._reset()
+    mock_randomiser.assert_called()
