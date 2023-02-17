893bedaad3f25a53d5604231b0dddcd28002ffc1
peaky76 <robertjamespeacock@gmail.com>
2023-02-17 13:24:40 +0000

    refactor: make Coin subclass of Randomiser

--- tabletoppy/coin.py
+++ tabletoppy/coin.py
@@ -1,9 +1,10 @@
 from enum import Enum
-import random
 from typing import Self
 
+from tabletoppy.randomisable import Randomisable
 
-class Coin:
+
+class Coin(Randomisable):
     """A class used to represent a coin with two sides, heads and tails"""
 
     Face = Enum("Face", ["HEADS", "TAILS"])
@@ -15,28 +16,19 @@ class Coin:
         :param face: The face currently displayed to the world, defaults to None, which will result in heads or tails being set at random
         :type face: Optional[Face], optional
         """
-        self.face = face
+        super().__init__(Coin.Face, face)
 
     def __str__(self):
         return f"Coin showing {self.face.name}"
 
-    def get_face(self) -> Face:  # type: ignore
-        """Getter for the face attribute
+    @property
+    def face(self) -> Face:
+        """The face currently displayed to the world
 
         :return: The face currently displayed to the world
         :rtype: Face
         """
-        return self._face
-
-    def set_face(self, value: Face | None = None) -> None:
-        """Setter for the face attribute
-
-        :param value: The face currently displayed to the world, defaults to None, which will result in heads or tails being set at random
-        :type value: Optional[Face], optional
-        """
-        self._face = value or Coin.Face(random.randint(1, 2))
-
-    face = property(get_face, set_face)
+        return self.selection
 
     def is_heads(self) -> bool:
         """Convenience method for checking whether the coin is showing heads
@@ -60,7 +52,7 @@ class Coin:
         :return: The same coin with either heads or tails showing, determined randomly
         :rtype: Self
         """
-        self.face = None
+        self._reset()
         return self
 
     def turn(self) -> Self:
@@ -69,8 +61,8 @@ class Coin:
         :return: The turned coin with the other face on display
         :rtype: Self
         """
-        if self.face == Coin.Face.HEADS:
-            self.face = Coin.Face.TAILS
+        if self.selection == Coin.Face.HEADS:
+            self.selection = Coin.Face.TAILS
         else:
-            self.face = Coin.Face.HEADS
+            self.selection = Coin.Face.HEADS
         return self
--- tests/test_coin.py
+++ tests/test_coin.py
@@ -1,4 +1,6 @@
+import random
 from tabletoppy import Coin
+
 
 [HEADS, TAILS] = Coin.Face
 
@@ -16,20 +18,6 @@ def test_initialize_coin_with_tails():
 def test_initialize_coin_default_produces_either_heads_or_tails():
     coin = Coin()
     assert coin.face in [HEADS, TAILS]
-
-
-def test_initialize_coin_default_randomly_determines_heads_or_tails(mocker):
-    mock_random = mocker.patch("tabletoppy.coin.random")
-    mocker.patch("tabletoppy.coin.Coin.Face")
-    Coin()
-    mock_random.randint.assert_called_with(1, 2)
-
-
-def test_initialize_coin_default_sets_heads_or_tails_as_randomly_generated(mocker):
-    mock_random = mocker.patch("tabletoppy.coin.random")
-    mock_random.randint.return_value = 1
-    coin = Coin()
-    assert HEADS == coin.face
 
 
 def test_str_representation_of_coin_is_as_intended():
@@ -59,12 +47,11 @@ def test_is_tails_returns_false_when_coin_face_is_heads():
 
 
 def test_toss_randomly_determines_heads_or_tails(mocker):
+    mock_randomiser = mocker.patch("tabletoppy.coin.Coin._randomiser")
+    mock_randomiser.return_value = 1
     coin = Coin()
-    mock_random = mocker.patch("tabletoppy.coin.random")
-    mock_face = mocker.patch("tabletoppy.coin.Coin.Face")
     coin.toss()
-    mock_random.randint.assert_called_with(1, 2)
-    mock_face.assert_called()
+    mock_randomiser.assert_called()
 
 
 def test_turn_coin_displaying_heads_puts_tails_on_display():
