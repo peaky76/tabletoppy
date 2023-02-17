7e06ab284bd5fb23a5a45677c6ab6fcc4d6b3deb
peaky76 <robertjamespeacock@gmail.com>
2023-02-17 17:37:16 +0000

    refactor: make Die subclass of Randomiser

--- tests/test_die.py
+++ tests/test_die.py
@@ -31,27 +31,9 @@ def test_initialize_die_with_face_correctly_sets_face():
     assert 6 == die.face
 
 
-def test_initialize_randomly_determines_face(mocker):
-    mock_random = mocker.patch("tabletoppy.die.random")
-    Die()
-    mock_random.randint.assert_called_with(1, 6)
-
-
-def test_initialize_uses_sides_as_randint_limit(mocker):
-    mock_random = mocker.patch("tabletoppy.die.random")
-    Die(8)
-    mock_random.randint.assert_called_with(1, 8)
-
-
-def test_initialize_sets_face_as_randomly_generated(mocker):
-    mock_random = mocker.patch("tabletoppy.die.random")
-    mock_random.randint.return_value = 4
-    die = Die()
-    assert 4 == die.face
-
-
 def test_roll_randomly_determines_a_face_to_display(mocker):
+    mock_randomiser = mocker.patch("tabletoppy.die.Die._randomiser")
+    mock_randomiser.return_value = 1
     die = Die()
-    mock_random = mocker.patch("tabletoppy.die.random")
     die.roll()
-    mock_random.randint.assert_called_with(1, 6)
+    mock_randomiser.assert_called()
