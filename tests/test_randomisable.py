272fd3a337abb0f22a2b488cb1742c838d1a161d
peaky76 <robertjamespeacock@gmail.com>
2023-02-17 17:14:36 +0000

    refactor: make Randomisable selection protected

--- tests/test_randomisable.py
+++ tests/test_randomisable.py
@@ -13,7 +13,7 @@ def test_initialize_randomisable_correctly_sets_options():
 
 def test_initialize_randomisable_correctly_sets_selection():
     randomisable = Randomisable(Foobar, Foobar.ALPHA)
-    assert Foobar.ALPHA == randomisable.selection
+    assert Foobar.ALPHA == randomisable._selection
 
 
 def test_initialize_randomisable_with_invalid_selection_raises_type_error():
@@ -32,7 +32,7 @@ def test_initialize_randomisable_determines_selection_as_randomly_generated(mock
     mock_randomiser = mocker.patch("tabletoppy.randomisable.Randomisable._randomiser")
     mock_randomiser.return_value = 2
     randomisable = Randomisable(Foobar)
-    assert Foobar.BETA == randomisable.selection
+    assert Foobar.BETA == randomisable._selection
 
 
 def test_randomiser_generates_random_integer_with_options_as_limit(mocker):
