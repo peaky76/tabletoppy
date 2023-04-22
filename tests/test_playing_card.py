3f359236a34dfba67f5d14ad56c902c10a06f98e
peaky76 <robertjamespeacock@gmail.com>
2023-04-22 22:52:26 +0100

    feat: add Joker to PlayingCard

--- tests/test_playing_card.py
+++ tests/test_playing_card.py
@@ -1,3 +1,4 @@
+import pytest
 from tabletoppy.playing_card import PlayingCard
 from tabletoppy import Suit
 
@@ -8,3 +9,12 @@ def test_playing_card_can_be_initialised_with_int_value_and_suit():
 
 def test_playing_card_can_be_initialised_with_str_value_and_suit():
     assert PlayingCard("Ace", Suit.HEARTS)
+
+
+def test_playing_card_can_be_initialised_with_joker_and_no_suit():
+    assert PlayingCard("Joker")
+
+
+def test_playing_card_cannot_be_initialised_with_any_other_value_without_suit():
+    with pytest.raises(TypeError):
+        assert PlayingCard("Ace")
