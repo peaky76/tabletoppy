02ee683c3a3c535a0e7a29bc5cdaab6ced3e39f9
peaky76 <robertjamespeacock@gmail.com>
2023-04-22 23:00:56 +0100

    docs: docstrings for PlayingCard

--- tabletoppy/playing_card.py
+++ tabletoppy/playing_card.py
@@ -6,7 +6,17 @@ _E = TypeVar("_E", bound=Enum)
 
 
 class PlayingCard:
+    """A class used to represent a playing card
+
+    :raises TypeError: If a joker is initialised with a suit
+    :raises TypeError: If a non-joker playing card is initialised without a suit
+    :return: A playing card
+    :rtype: PlayingCard
+    """
+
     class Value(Enum):
+        """An Enum representing the values of a playing card"""
+
         JOKER = 0
         ACE = 1
         TWO = 2
@@ -23,15 +33,28 @@ class PlayingCard:
         KING = 13
 
     def __init__(self, value: int | str, suit: _E | None = None):
+        """Initializer for a playing card object
+
+        :param value: The value of the playing card, can be either an int or a str
+        :type value: int or str
+        :param suit: The suit of the playing card, defaults to None
+        :type suit: Optional[_E], optional
+        """
         self.value = value
         self.suit = suit
 
     @property
     def suit(self) -> _E:
+        """Getter for the suit of the playing card
+
+        :return: The suit of the playing card
+        :rtype: _E
+        """
         return self._suit
 
     @suit.setter
     def suit(self, suit: _E | None) -> None:
+        """Setter for the suit of the playing card"""
         if self.value is PlayingCard.Value.JOKER and suit is not None:
             raise TypeError("Joker cannot have a suit")
         if self.value is not PlayingCard.Value.JOKER and suit is None:
@@ -40,10 +63,16 @@ class PlayingCard:
 
     @property
     def value(self) -> Value:
+        """Getter for the value of the playing card
+
+        :return: The value of the playing card
+        :rtype: Value
+        """
         return self._value
 
     @value.setter
     def value(self, value: Value) -> None:
+        """Setter for the value of the playing card"""
         if isinstance(value, int):
             self._value = PlayingCard.Value(value)
         else:
