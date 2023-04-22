c43e6e4c21d327b6766ce466d8d06d611dd6edda
peaky76 <robertjamespeacock@gmail.com>
2023-04-22 23:31:23 +0100

    feat: add PlayingCard.__str__

--- tests/test_playing_card.py
+++ tests/test_playing_card.py
@@ -18,3 +18,15 @@ def test_playing_card_can_be_initialised_with_joker_and_no_suit():
 def test_playing_card_cannot_be_initialised_with_any_other_value_without_suit():
     with pytest.raises(TypeError):
         assert PlayingCard("Ace")
+
+
+def test_playing_card_string_representation_for_numerals():
+    assert str(PlayingCard(10, Suit.SPADES)) == "10♠"
+
+
+def test_playing_card_string_representation_for_special_cards():
+    assert str(PlayingCard("Ace", Suit.SPADES)) == "A♠"
+
+
+def test_playing_card_string_representation_for_joker():
+    assert str(PlayingCard("Joker")) == "Joker"
