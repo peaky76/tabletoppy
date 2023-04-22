a6713a09435b720c0f04d6817026ecebbed25ac2
peaky76 <robertjamespeacock@gmail.com>
2023-04-22 22:52:26 +0100

    feat: add Joker to PlayingCard

--- tabletoppy/playing_card.py
+++ tabletoppy/playing_card.py
@@ -7,6 +7,7 @@ _E = TypeVar("_E", bound=Enum)
 
 class PlayingCard:
     class Value(Enum):
+        JOKER = 0
         ACE = 1
         TWO = 2
         THREE = 3
@@ -21,7 +22,7 @@ class PlayingCard:
         QUEEN = 12
         KING = 13
 
-    def __init__(self, value: int | str, suit: _E):
+    def __init__(self, value: int | str, suit: _E | None = None):
         self.value = value
         self.suit = suit
 
@@ -30,7 +31,11 @@ class PlayingCard:
         return self._suit
 
     @suit.setter
-    def suit(self, suit: _E) -> None:
+    def suit(self, suit: _E | None) -> None:
+        if self.value is PlayingCard.Value.JOKER and suit is not None:
+            raise TypeError("Joker cannot have a suit")
+        if self.value is not PlayingCard.Value.JOKER and suit is None:
+            raise TypeError("Playing card must have a suit")
         self._suit = suit
 
     @property
