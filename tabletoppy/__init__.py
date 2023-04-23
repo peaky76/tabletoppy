3a88165f1fc758ab8016d51bdf94fa633f7741ff
peaky76 <robertjamespeacock@gmail.com>
2023-04-23 11:30:08 +0100

    build: add Rank to package __init__

--- tabletoppy/__init__.py
+++ tabletoppy/__init__.py
@@ -1,6 +1,7 @@
 from .coin import Coin
 from .die import Die
 from .playing_card import PlayingCard
+from .rank import Rank
 from .suit import Suit
 
-__all__ = ["Coin", "Die", "PlayingCard", "Suit"]
+__all__ = ["Coin", "Die", "PlayingCard", "Rank", "Suit"]
