07c6ec1fe6e86944e8426408a6160c4fcb83927b
peaky76 <robertjamespeacock@gmail.com>
2023-04-23 11:06:49 +0100

    feat: add Deck class with init for 52 cards

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -4,8 +4,3 @@ from tabletoppy.deck import Deck
 def test_deck_init_without_jokers_creates_52_cards():
     deck = Deck()
     assert 52 == len(deck.cards)
-
-
-def test_deck_init_with_jokers_creates_54_cards():
-    deck = Deck(jokers=2)
-    assert 54 == len(deck.cards)
