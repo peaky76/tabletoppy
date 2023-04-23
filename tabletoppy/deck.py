5aef01688b10e7c829e1fd84ef078846ff3a8d94
peaky76 <robertjamespeacock@gmail.com>
2023-04-23 21:29:43 +0100

    feat: Deck init with multiple packs

--- tabletoppy/deck.py
+++ tabletoppy/deck.py
@@ -4,13 +4,15 @@ from .suit import Suit
 
 
 class Deck:
-    def __init__(self, jokers: int = 0):
+    def __init__(self, jokers: int = 0, packs: int = 1):
         """Initializes a standard deck of playing cards, default is a standard 52 card deck
 
-        param jokers: The number of jokers to add to the deck, defaults to 0
-        type jokers: int, optional
+        :param jokers: The number of jokers to add to the deck, defaults to 0
+        :type jokers: int, optional
+        :param packs: The number of packs to add to the deck, defaults to 1
+        :type packs: int, optional
         """
-        regular_cards = [Card(val, suit) for val in range(1, 14) for suit in Suit]
+        regular_cards = [Card(n, suit) for n in range(1, 14) for suit in Suit] * packs
         added_cards = [Card("Joker")] * jokers
         self.cards = deque(regular_cards + added_cards)
 
