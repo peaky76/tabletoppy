0797b8920f6378fec55b358e14b6068e14e29561
peaky76 <robertjamespeacock@gmail.com>
2023-04-23 12:07:32 +0100

    feat: make Deck.cards a deque

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -9,3 +9,18 @@ def test_deck_init_without_jokers_creates_52_cards():
 def test_deck_init_with_jokers_creates_54_cards():
     deck = Deck(jokers=2)
     assert 54 == len(deck.cards)
+
+
+def test_deck_first_card_is_ace_of_spades_in_standard_deck_with_standard_order():
+    deck = Deck()
+    assert "A♠" == str(deck.cards.popleft())
+
+
+def test_deck_last_card_is_king_of_diamonds_in_standard_deck_with_standard_order():
+    deck = Deck()
+    assert "K♦" == str(deck.cards.pop())
+
+
+def test_deck_last_card_is_joker_in_standard_deck_with_jokers_and_standard_order():
+    deck = Deck(jokers=2)
+    assert "Joker" == str(deck.cards.pop())
