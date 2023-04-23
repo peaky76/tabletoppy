a737a8495e538130888bb99d9530a4a52e204a15
peaky76 <robertjamespeacock@gmail.com>
2023-04-23 12:07:32 +0100

    feat: make Deck.cards a deque

--- tabletoppy/deck.py
+++ tabletoppy/deck.py
@@ -1,3 +1,4 @@
+from collections import deque
 from .playing_card import PlayingCard as Card
 from .suit import Suit
 
@@ -9,9 +10,9 @@ class Deck:
         param jokers: The number of jokers to add to the deck, defaults to 0
         type jokers: int, optional
         """
-        self.cards = [Card(val, suit) for val in range(1, 14) for suit in Suit] + (
-            [Card("Joker")] * jokers
-        )
+        regular_cards = [Card(val, suit) for val in range(1, 14) for suit in Suit]
+        added_cards = [Card("Joker")] * jokers
+        self.cards = deque(regular_cards + added_cards)
 
     def cut(self):
         pass
